efd6cc4db8743eb11a88d349fdac23e672e19781
jie liu <jie1.liu@bkjk.com>
2018-05-15 16:17:07 +0800

    27. 移除元素

--- easy/array/26. Remove Duplicates from Sorted Array.py
+++ easy/array/26. Remove Duplicates from Sorted Array.py
@@ -3,7 +3,6 @@
     Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
 
     Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
-
     Example 1:
 
     Given nums = [1,1,2],
